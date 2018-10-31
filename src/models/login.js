451708108140cb7fc39ed9c3d48aa009407029fe
Alvaro Willams Alejandro Arancibia <aljand321@gmail.com>
2018-10-30 22:41:25 -0400

    añadiendo login

---
+++ src/models/login.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const loginSchema = new Schema ({
+
+  nombres: String,
+  apellidos:String,
+  email: String,
+  password: String
+  telefono: Number,
+  ci: String
+
+});
+
+loginSchema.virtual("password_confirmation").get(function(){
+  return this.p_c;
+
+}).set(function(password){
+  this.p_c = password;
+});
+
+module.exports = mongoose.model('login', loginSchema);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Estudiantes = require('../models/estudiantes');
+const Login = require('../models/login');
 
 router.get('/', async (req, res) => {
   const estudiantes = await Estudiantes.find();
@@ -34,6 +35,26 @@ router.post('/editEst/:id', async (req, res) => {
 });
 
 //>>>>>>>>>>>>>>>>
+
+//>>>>>>>>>
+// // servicio para login
+// <<<<<<<<<<<<<<<<<<<<
+router.post("/users", function(req, res) {
+  var user = new Login({email: req.body.email, password: req.body.password, password_confirmation: req.body.password_confirmation});
+  user.save(function(err){
+    res.send("se guardo los datos que mandaste");
+  });
+});
+// >>>>>>>>>>>>>>>>>>>>
+//servicio para mostrar datos del login
+router.get("/user", function(req, res){
+  Login.find(function(err, doc){
+    console.log(doc);
+    res.render("user");
+  });
+});
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
 
 router.get('/turn/id:', async(req, res)=> {
   const { id }= req.params;
