c076316fab57370901b93126d5efeefbf6df1235
Alvaro Willams Alejandro Arancibia <aljand321@gmail.com>
2018-11-14 20:05:06 -0400

    solo commit

--- src/routes/files.js
+++ src/routes/files.js
@@ -2,6 +2,7 @@ var express = require('express')
 var fs = require('fs')
 var path = require('path')
 var util = require('util')
+const Pdf = require('../models/pdf');
 var router = express.Router()
 
 function filterDotFiles (files) {
@@ -9,7 +10,15 @@ function filterDotFiles (files) {
 }
 
 router.get('/', function (req, res, next) {
-  const testFolder = path.join(__dirname, '../public/documents')
+
+  var data = req.query;
+  Pdf.find({_id:data.id}).exec(function(err, docs) {
+    var name = docs.physicalpath.split("/")[2];
+    //res.redirect("http://localhost:3000/documents/"+name);
+    res.send("http://localhost:3000/documents/"+name);
+  });
+  /*
+  const testFolder = path.join('/home/alejandro/vicerrectorado2.0/public/documents/')
   var readdir = util.promisify(fs.readdir)
 
   return readdir(testFolder)
@@ -17,7 +26,7 @@ router.get('/', function (req, res, next) {
     .then(files => {
       res.render('files.ejs', {files})
     })
-    .catch(next)
+    .catch(next)*/
 })
 
 module.exports = router;
